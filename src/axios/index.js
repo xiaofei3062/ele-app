10eddec494124092d17764443bcd02b85edee047
1592826708@qq.com <2165373shen>
2019-12-25 17:30:20 +0800

    饿了么

--- src/axios/index.js
+++ src/axios/index.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import { Toast } from "vant";
 
 let config = {
-  // baseURL: "http://127.0.0.1:5000"
+  // baseURL: "https://ele-interface.herokuapp.com"
 };
 
 const _axios = axios.create(config);
