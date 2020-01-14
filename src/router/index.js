78b9925c74f2890b83a32284ce033301d3e2c43a
小飞飞 <1592826708@qq.com>
2020-01-14 21:47:16 +0800

    饿了么

--- src/router/index.js
+++ src/router/index.js
@@ -27,12 +27,9 @@ const router = new VueRouter({
 // 路由全局守卫
 router.beforeEach((to, from, next) => {
   const isLogin = !!localStorage.ele_login;
-  if (to.path === "/login") {
-    next();
-  } else {
-    // 判断是否在登录状态下
-    isLogin ? next() : next("/login");
-  }
+  if (to.path === "/login") return next();
+  // 判断是否在登录状态下
+  isLogin ? next() : next("/login");
 });
 
 export default router;
